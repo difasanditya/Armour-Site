c2cabb7fd6cc7f66bf1f824357ef5adf2a85324b
alfianwi97 <alfian.wijayakusuma@yahoo.com>
2017-08-12 00:44:15 +0700

    Member section update
    Fix hover effect on member section

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,40 +1,44 @@
 var incrementPoint=parseInt($(".photo-bar").css("height"));
-var delay = 1000;
 var viewPosition = 0;
+var onSlide=false;
 
 function hoverUp(){
     if(viewPosition<1) return;
+    onSlide=true;
     viewPosition--;
     var position = parseInt($(".photo-bar").css("bottom"));
     position-=incrementPoint;
     $('.photo-bar').stop().animate({
         bottom: position    
-    }, delay);
+    }, 500);
     setTimeout(function(){
         if ($('#arrow-up:hover').length != 0) {
             hoverUp();
-        }
-    },delay)
+        }else onSlide=false;
+    },1000)
 }
 
 function hoverDown(){
     if(viewPosition>=3) return;
+    onSlide=true;
     viewPosition++;
     var position = parseInt($(".photo-bar").css("bottom"));
     position+=incrementPoint;
     $('.photo-bar').stop().animate({
         bottom: position    
-    }, delay);
+    }, 500);
     setTimeout(function(){
         if ($('#arrow-down:hover').length != 0) {
             hoverDown();   
-        }
-    },delay)
+        }else onSlide=false;
+    },1000)
 }
 
 $('#arrow-up').mouseover(function(){
+    if(!onSlide)
     hoverUp();
 });
 $('#arrow-down').mouseover(function(){
+    if(!onSlide)
     hoverDown();
 });
