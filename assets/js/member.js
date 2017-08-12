c02102b8d35d6da5ccbfa44bf14a64daa9136f11
alfianwi97 <alfian.wijayakusuma@yahoo.com>
2017-08-12 13:33:53 +0700

    Member section update
    Finishing member section features: hover  and slide effect. Add armour-member images.

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,9 +1,13 @@
+/////////////////////slide-effect/////////////////////////
 var incrementPoint=parseInt($(".photo-bar").css("height"));
 var viewPosition = 0;
 var onSlide=false;
 
 function hoverUp(){
-    if(viewPosition<1) return;
+    if(viewPosition<1){
+        onSlide=false
+        return;
+    }
     onSlide=true;
     viewPosition--;
     var position = parseInt($(".photo-bar").css("bottom"));
@@ -19,7 +23,10 @@ function hoverUp(){
 }
 
 function hoverDown(){
-    if(viewPosition>=3) return;
+    if(viewPosition>=3){
+        onSlide=false
+        return;
+    }
     onSlide=true;
     viewPosition++;
     var position = parseInt($(".photo-bar").css("bottom"));
@@ -42,3 +49,48 @@ $('#arrow-down').mouseover(function(){
     if(!onSlide)
     hoverDown();
 });
+///////////////////end-of-slide-effect/////////////
+///////////////////photo-init/////////////////////
+var armourMemberCount = 28;
+var path = "assets/img/armour-member/";
+var photoNames = [];
+for(var i=1;i<=armourMemberCount;i++){
+    var string;
+    var temp=parseInt(i);
+    if(temp<10) string="00"+i+".jpg";
+    else string="0"+i+".jpg";
+    photoNames.push(string);
+}
+
+var frames = $('.mini-photo');
+var url;
+for(var i=0; i<frames.length; i++){
+    url = path+photoNames[i];
+    frames.eq(i).css('backgroundImage','url("'+url+'")');
+    //document.getElementsByClassName("mini-photo")[i].style.backgroundImage="url('"+url+"')";
+}
+////////////////////end-of-photo-init////////////
+////////////////////photo-effect/////////////////
+var onOverStatus=false;
+var currentIndex;
+for(var i=0; i<frames.length; i++){
+    var element = frames.eq(i);
+    element.mouseover(function(){
+        if(onOverStatus) return;
+        onOverStatus=true;
+      //  currentIndex=frames.index(this);
+        $("#frame").fadeOut(500);
+        setTimeout(function(){
+            url = path+photoNames[currentIndex];
+            $("#frame").css('backgroundImage','url("'+url+'")');
+            $("#frame").fadeIn(1000);
+            onOverStatus=false;
+            //setTimeout(function(){onOverStatus=false;},500);
+        },1000)
+        //$("#frame").css('opacity','0');
+    });
+    element.mouseenter(function(){
+        currentIndex=frames.index(this);
+    });
+}
+///////////////////end-of-photo-effect///////////
